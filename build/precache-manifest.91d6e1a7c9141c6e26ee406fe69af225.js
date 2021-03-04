12c3889b7de22554bb715e08bc725d3c12547732
darp-cs <diegoarod14@gmail.com>
2021-03-04 10:37:51 -0500

    Adding guessing game

---
+++ build/precache-manifest.91d6e1a7c9141c6e26ee406fe69af225.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "113f1fd596951d9452c63241695f6e60",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a142ec3e67c980da509f",
+    "url": "/static/css/main.c21035f2.chunk.css"
+  },
+  {
+    "revision": "8185f92e89bd6d09ea3b",
+    "url": "/static/js/2.62612992.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.62612992.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a142ec3e67c980da509f",
+    "url": "/static/js/main.73bd9a70.chunk.js"
+  },
+  {
+    "revision": "fb23d6b5906dd033e185",
+    "url": "/static/js/runtime-main.dec1aa00.js"
+  },
+  {
+    "revision": "88d4da8b2ed3cf7bb1929bc4c25d10ca",
+    "url": "/static/media/confetti.88d4da8b.gif"
+  },
+  {
+    "revision": "be71a5fb3c920a7d03e4f402c53d3ed6",
+    "url": "/static/media/fellowships.be71a5fb.png"
+  },
+  {
+    "revision": "175c9daeab6543a7266db3c5211674de",
+    "url": "/static/media/r2h.175c9dae.png"
+  }
+]);
---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Board from './Components/Board/index'
+
+
+function App() {
+  return (
+    <>
+      <Board />
+    </>
+  );
+}
+
+export default App;
